a77f0bac0cb4c203a457a27361b49dcea583e2c6
zhouwei2018 <zhouwei163mail@163.com>
2017-12-27 02:25:21 +0800

    ++

--- config/domain.js
+++ config/domain.js
@@ -1,6 +1,6 @@
 const domainInfo = {
-    mainDomain:"http://127.0.0.1/server",
-    testDomain:"http://127.0.0.1/server",
+  mainDomain:"http://47.92.24.140/server",
+  testDomain:"http://47.92.24.140/server",
 };
 
 export default domainInfo
