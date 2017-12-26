19557f14ad3cbb0137330f92b4e6d2dcadcd5eb4
zhouwei2018 <zhouwei163mail@163.com>
2017-12-27 01:51:01 +0800

    ++

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -13,14 +13,28 @@ var app = new Vue({
 
     },
     components: {
-        footerview:footerview,
-        headerview:headerview
+        footerview: footerview,
+        headerview: headerview
     },
-    created:function(){},
-    mounted:function () {
+    created: function () {
+    },
+    mounted: function () {
+        $('.about_slide_list').eq(0).show();
 
+        $('html body').click(function(){
+            $('#about_head_tab').slideUp(100);
+        });
     },
     methods: {
+        showHideUl(e){
+            $(e.target).parents('.banner_switch').find('ul').slideToggle(100);
+            $(e.target).parents('.banner_switch').find('i').toggleClass('down');
+        },
 
+        term_switch(e){
+            var index=$(e.target).index();
+            $(e.target).addClass('active').siblings().removeClass('active');
+            $('.about_slide_list').eq(index).show().siblings().hide();
+        }
     }
 });
